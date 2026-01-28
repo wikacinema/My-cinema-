366bac40a5ce249555ebdc5f077d3c23a4e37719
wikacinema <tv.hussien.tv@gmail.com>
2026-01-28 16:08:32 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,35 @@
+const CACHE_NAME = 'wika-cinema-v3';
+const assets = [
+    '/My-cinema-/',
+    '/My-cinema-/index.html',
+    '/My-cinema-/manifest.json',
+    'https://i.ibb.co/zTr7jm5M/Picsart-26-01-16-01-39-26-682.png'
+];
+
 self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    // ملف فارغ لغرض تفعيل التثبيت فقط
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
